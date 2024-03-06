a851a3ad79f54e31adf40b0d3df34b58cabd40c1
gorhf9397 <trr456@naver.com>
2024-03-06 23:17:56 +0900

    Firebase CRUD 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { async } from "@firebase/util";
-import { doc, setDoc, addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  doc,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+  collection,
+  getDocs,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { dbService } from "./firebase";
 
 function App() {
   const [name, setName] = useState("");
   const [country, setCountry] = useState("");
-  const [obj, setObj] = useState("");
+  const [editing, setEditing] = useState("");
+  const [obj, setObj] = useState([]);
   const nameChange = (e) => {
     const {
       target: { value },
@@ -26,15 +33,38 @@ function App() {
       country: country,
     });
   };
+
+  const onUpdateClick = async (id) => {
+    if (editing === id) {
+      await updateDoc(doc(dbService, "cities", id), {
+        name: name,
+        country: country,
+      });
+      setEditing("");
+    } else {
+      setEditing(id);
+    }
+  };
+
+  const onDeleteClick = async (id) => {
+    const ok = window.confirm("정말 삭제하시겠습니까?");
+    if (ok) {
+      await deleteDoc(doc(dbService, "cities", id));
+    }
+  };
+
   useEffect(() => {
     const getData = async () => {
-      const query = await getDocs(collection(dbService, "cities"));
-      query.forEach((e) => setObj(e.data()));
+      const querySnapshot = await getDocs(collection(dbService, "cities"));
+      const cityArray = []; // 새 배열 생성
+      querySnapshot.forEach((doc) => {
+        cityArray.push(doc); // 배열에 각 문서의 데이터 추가
+      });
+      setObj(cityArray); // 배열을 상태에 설정
     };
     getData();
-    console.log(obj);
   }, []);
-  console.log(obj.name);
+
   return (
     <div>
       <input onChange={nameChange} type="text" />
@@ -44,8 +74,34 @@ function App() {
       <button onClick={onClick} type="submit">
         gogo
       </button>
+      <button>getData</button>
 
-      <div></div>
+      {obj.map((city) => (
+        <div key={city.id}>
+          {editing === city.id ? (
+            <>
+              <input
+                type="text"
+                placeholder="도시 이름"
+                value={name}
+                onChange={nameChange}
+              />
+              <input
+                type="text"
+                placeholder="국가"
+                value={country}
+                onChange={countryChange}
+              />
+            </>
+          ) : (
+            <h3>
+              {city.id} - {city.data().name} - {city.data().country}
+            </h3>
+          )}
+          <button onClick={() => onUpdateClick(city.id)}>수정</button>
+          <button onClick={() => onDeleteClick(city.id)}>삭제</button>
+        </div>
+      ))}
     </div>
   );
 }
